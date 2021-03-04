d3e388638762303a5b7c8c3d038aa57cb98cc71c
laynath242@gmail.com <laynath242@gmail.com>
2021-03-04 09:23:19 +0700

    update

--- attendant_token/lib.rs
+++ attendant_token/lib.rs
@@ -5,7 +5,7 @@ use ink_lang as ink;
 #[ink::contract]
 mod attendant_token {
 
-    use ink_prelude::{string::String, vec::Vec};
+    use ink_prelude::{string::String, vec, vec::Vec};
     use ink_storage::collections::HashMap as StorageHashMap;
     use ink_storage::traits::{PackedLayout, SpreadLayout};
 
@@ -15,6 +15,8 @@ mod attendant_token {
         InvalidHash,
         CheckOutFail,
         CheckInFail,
+        ChangeFail,
+        PermissionDeny,
     }
 
     #[derive(Debug, scale::Encode, PackedLayout, scale::Decode, Clone, SpreadLayout)]
@@ -35,18 +37,24 @@ mod attendant_token {
         check_out: StorageHashMap<AccountId, Vec<Attendant>>,
         check_in_status: StorageHashMap<AccountId, bool>,
         check_in_time: StorageHashMap<AccountId, u64>,
+        change_able: bool,
+        owner: AccountId,
     }
 
     impl AttendantToken {
         #[ink(constructor)]
-        pub fn new(attendant_hash: Hash) -> Self {
+        pub fn new() -> Self {
+            let hash = Hash::from([1; 32]);
+            let caller = Self::env().caller();
             Self {
                 token: StorageHashMap::new(),
-                attendant_hash,
+                attendant_hash: hash,
                 check_in: StorageHashMap::new(),
                 check_out: StorageHashMap::new(),
                 check_in_status: StorageHashMap::new(),
                 check_in_time: StorageHashMap::new(),
+                change_able: false,
+                owner: caller,
             }
         }
 
@@ -72,10 +80,13 @@ mod attendant_token {
                 attendant_list.push(info);
                 self.check_in.insert(caller, attendant_list);
 
-                let token = self.get_attendant_token(caller);
+                let token = self.get_token(caller);
                 self.token.insert(caller, token + 8);
 
                 self.check_in_status.insert(caller, true);
+                if self.change_able {
+                    self.generate_hash();
+                };
                 Ok(())
             }
         }
@@ -103,17 +114,20 @@ mod attendant_token {
 
                 let hour = 1000 * 60 * 60;
                 if now - self.get_checked_time(caller) > hour * 8 {
-                    let token = self.get_attendant_token(caller);
+                    let token = self.get_token(caller);
                     self.token.insert(caller, token + 2);
                 }
 
                 self.check_in_status.insert(caller, false);
+                if self.change_able {
+                    self.generate_hash();
+                };
                 Ok(())
             }
         }
 
         #[ink(message)]
-        pub fn get_attendant_token(&self, attender: AccountId) -> Balance {
+        pub fn get_token(&self, attender: AccountId) -> Balance {
             self.token.get(&attender).copied().unwrap_or(0)
         }
 
@@ -142,15 +156,30 @@ mod attendant_token {
         }
 
         #[ink(message)]
-        pub fn change_attendant_hash(&mut self, new_hash: Hash) {
+        pub fn is_changeable(&mut self, can: bool)-> Result<(), Error>  {
+            let caller = self.env().caller();
+            if caller == self.owner {
+                self.change_able = can;
+                Ok(())
+            }else {
+                Err(Error::PermissionDeny)
+            }
+            
+        }
+
+        #[ink(message)]
+        pub fn get_hash(&self) -> Hash {
+            self.attendant_hash
+        }
+
+        fn generate_hash(&mut self) {
+            let x = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 34, 56, 78, 90];
+            let new_hash = self.env().random(&x);
             self.attendant_hash = new_hash;
         }
 
         fn get_checked_time(&self, attender: AccountId) -> u64 {
-            self.check_in_time
-                .get(&attender)
-                .copied()
-                .unwrap_or(0)
+            self.check_in_time.get(&attender).copied().unwrap_or(0)
         }
     }
 }
